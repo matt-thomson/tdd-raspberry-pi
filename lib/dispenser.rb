03f2091e98d03d8fb7868c5674c97330c4082ed3
Matt Thomson <matt.thomson@cantab.net>
2013-06-25 18:39:24 +0100

    Don't dispense if the bin is full

--- lib/dispenser.rb
+++ lib/dispenser.rb
@@ -6,7 +6,7 @@ class Dispenser
 	end
 
 	def tick
-		if @button_adapter.is_pressed?
+		if @button_adapter.is_pressed? and !@bin_adapter.is_down?
 			@motor_adapter.turn_on
 		else
 			@motor_adapter.turn_off
--- test/dispenser_test.rb
+++ test/dispenser_test.rb
@@ -2,13 +2,25 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 require 'dispenser'
 
 class MotorTest < Test::Unit::TestCase
-	def test_turns_on_motor_when_button_pressed
+	def test_turns_on_motor_when_button_pressed_and_bin_empty
 		button_adapter = stub(:is_pressed? => true)
 
 		motor_adapter = mock("a motor adapter")
 		motor_adapter.expects(:turn_on)
 
-        bin_adapter = stub()
+        bin_adapter = stub(:is_down? => false)
+
+		motor = Dispenser.new(button_adapter, motor_adapter, bin_adapter)
+		motor.tick
+	end
+
+	def test_turns_off_motor_when_button_pressed_and_item_in_bin
+		button_adapter = stub(:is_pressed? => true)
+
+		motor_adapter = mock("a motor adapter")
+		motor_adapter.expects(:turn_off)
+
+        bin_adapter = stub(:is_down? => true)
 
 		motor = Dispenser.new(button_adapter, motor_adapter, bin_adapter)
 		motor.tick
